e872ed2147670f0088f6683a1c5e8370ce7c14c2
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-06-29 00:46:35 +0700

    feat: setup router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// create a new router instance
+const router = createRouter({ routeTree })
+
+// register the router instance for the type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
