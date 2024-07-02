cc14e281d738060145d5248a8fb181a68e56fe6e
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-07-02 22:19:35 +0700

    feat: init dashboard, navigation bar, add dayjs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,49 @@
 import { createFileRoute } from "@tanstack/react-router";
 import AppLayout from "../components/AppLayout";
+import dayjs from "dayjs";
+import NavigationBar from "../components/NavigationBar";
 
 export const Route = createFileRoute("/")({
   component: IndexPage,
 });
 
 function IndexPage() {
-  return <AppLayout>hehehehe</AppLayout>;
+  const data = [
+    {
+      id: 4,
+      user_id: 1,
+      type: "outflow",
+      amount: 14000,
+      category: "makan",
+      date: "2024-06-27",
+      note: "ketoprak",
+      is_active: true,
+      created_at: "2024-06-26T17:20:48.090Z",
+      updated_at: "2024-06-26T17:20:48.090Z",
+    },
+    {
+      id: 3,
+      user_id: 1,
+      type: "outflow",
+      amount: 20000,
+      category: "makan",
+      date: "2024-06-27",
+      note: "hejo",
+      is_active: true,
+      created_at: "2024-06-26T17:20:27.973Z",
+      updated_at: "2024-06-27T02:14:49.762Z",
+    },
+  ];
+
+  const today = dayjs();
+  return (
+    <AppLayout>
+      <div className="h-full flex flex-col justify-between">
+        <div className="text-center">{today.format("dddd, DD MMMM YYYY")}</div>
+        <p>hehehe</p>
+        {/*  */}
+        <NavigationBar />
+      </div>
+    </AppLayout>
+  );
 }
