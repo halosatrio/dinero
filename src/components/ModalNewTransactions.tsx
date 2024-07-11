da12b66281c2621471af75e47c92c5f47cd74d9b
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-07-12 00:59:57 +0700

    feat: implement endpoint create transaction

--- src/components/ModalNewTransactions.tsx
+++ src/components/ModalNewTransactions.tsx
@@ -11,25 +11,58 @@ import { CATEGORY } from "../helper/constant";
 import { IconCalendar } from "@tabler/icons-react";
 import { DateInput } from "@mantine/dates";
 import { useForm } from "@mantine/form";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import dayjs from "dayjs";
 
 type ModalNewTransactionProps = {
   open: boolean;
   close: () => void;
 };
 
+type NewTransactionSchema = {
+  type: "inflow" | "outflow";
+  amount: number;
+  category: string;
+  date: string | Date;
+  notes: string;
+};
+
 export default function ModalNewTransaction({
   open,
   close,
 }: ModalNewTransactionProps) {
-  const form = useForm({
-    initialValues: {
-      date: new Date(),
-      category: undefined,
-      type: "outflow",
-      amount: undefined,
-      notes: undefined,
+  const form = useForm<NewTransactionSchema>();
+
+  const { mutate } = useMutation({
+    mutationFn: async (bodyReq: NewTransactionSchema) => {
+      axios.post(
+        `${import.meta.env.VITE_API_URL}/transaction/create`,
+        {
+          ...bodyReq,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      close();
+      form.reset();
     },
   });
+  function handleSubmit(values: any) {
+    mutate({
+      type: values.type,
+      amount: values.amount,
+      category: values.category,
+      date: dayjs(values.date).format("YYYY-MM-DD"),
+      notes: values.notes,
+    });
+  }
+
   return (
     <Modal
       opened={open}
@@ -37,19 +70,20 @@ export default function ModalNewTransaction({
       title={<Title order={3}>New Transaction</Title>}
       centered
     >
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
         <Stack mt="sm" gap="lg">
           <DateInput
             required
             label="Select Date"
             valueFormat="DD MMM YYYY"
             leftSection={<IconCalendar />}
+            defaultValue={new Date()}
             {...form.getInputProps("date")}
           />
           <Select
             required
             label="Cash In/Out"
-            data={["Outflow", "Inflow"]}
+            data={["outflow", "inflow"]}
             placeholder="Cash In / Cash Out"
             {...form.getInputProps("type")}
           />
