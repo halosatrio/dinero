b0302e866c0ef7a74d82f5a7f68f0c093a713e12
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-07-21 13:10:22 +0700

    chores: someting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tsconfigPaths(),
+    visualizer() as PluginOption,
+  ],
 });
