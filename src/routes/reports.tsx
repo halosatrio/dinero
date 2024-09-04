97e098add97dd0a9fa3ab3fda9d6b320b5e2dbe7
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-09-05 00:14:08 +0700

    feat: card report dummy static data

--- src/routes/reports.tsx
+++ src/routes/reports.tsx
@@ -36,6 +36,24 @@ const PERIOD_DATA = [
   { value: "2025-1", label: "Q1 2025" },
 ];
 
+// const ESSENTIALS_ARRAY = ["Makan", "Cafe", "Errand", "Utils", "Bensin"]
+// const NON_ESSENTIALS_ARRAY = ["Misc", "Family", "Transport", "Travelling", "Date"]
+
+const dummy_essentials = [
+  { category: "Makan", amount: [1400500, 1322000, 1424000] },
+  { category: "Cafe", amount: [206000, 172000, 184000] },
+  { category: "Errand", amount: [353500, 407500, 331500] },
+  { category: "Utils", amount: [1943000, 1743000, 1732500] },
+  { category: "Bensin", amount: [120000, 160000, 160000] },
+];
+const dummy_non_essentials = [
+  { category: "Misc", amount: [435000, 145000, 8450000] },
+  { category: "Family", amount: [1307000, 300000, 500000] },
+  { category: "Transport", amount: [0, 0, 17000] },
+  { category: "Travelling", amount: [300000, 0, 0] },
+  { category: "Date", amount: [223000, 162000, 50000] },
+];
+
 function ReportPage() {
   const [value, setValue] = useState<string | null>("2024-3");
 
@@ -112,7 +130,7 @@ function ReportPage() {
           </Box>
         </Center>
 
-        {/* REGION: Monthly Transaction */}
+        {/* REGION: Report Card Essentials */}
         <Paper p="xs" shadow="md" mt="lg" withBorder>
           <Center mb="md" fw="bold" fz="h3">
             Essentials
@@ -121,72 +139,59 @@ function ReportPage() {
             <Table.Thead>
               <Table.Tr>
                 <Table.Th></Table.Th>
-                <Table.Th>January</Table.Th>
-                <Table.Th>February</Table.Th>
-                <Table.Th>March</Table.Th>
+                <Table.Th>July</Table.Th>
+                <Table.Th>August</Table.Th>
+                <Table.Th>September</Table.Th>
               </Table.Tr>
             </Table.Thead>
             <Table.Tbody>
+              {dummy_essentials.map((item) => (
+                <Table.Tr key={item.category}>
+                  <Table.Td fw={"bold"}>{item.category}</Table.Td>
+                  {item.amount.map((item, idx) => (
+                    <Table.Td key={idx}>
+                      {new Intl.NumberFormat("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                        minimumFractionDigits: 0,
+                      }).format(item)}
+                    </Table.Td>
+                  ))}
+                </Table.Tr>
+              ))}
+            </Table.Tbody>
+          </Table>
+        </Paper>
+
+        {/* REGION: Report Card Non-Essentials*/}
+        <Paper p="xs" shadow="md" mt="lg" withBorder>
+          <Center mb="md" fw="bold" fz="h3">
+            Non Essentials
+          </Center>
+          <Table>
+            <Table.Thead>
               <Table.Tr>
-                <Table.Td fw={"bold"}>Makan</Table.Td>
-                {[1400500, 1322000, 1424000].map((item, idx) => (
-                  <Table.Td key={idx}>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "IDR",
-                      minimumFractionDigits: 0,
-                    }).format(item)}
-                  </Table.Td>
-                ))}
+                <Table.Th></Table.Th>
+                <Table.Th>July</Table.Th>
+                <Table.Th>August</Table.Th>
+                <Table.Th>September</Table.Th>
               </Table.Tr>
-              <Table.Tr>
-                <Table.Td fw={"bold"}>Cafe</Table.Td>
-                {[206000, 172000, 184000].map((item, idx) => (
-                  <Table.Td key={idx}>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "IDR",
-                      minimumFractionDigits: 0,
-                    }).format(item)}
-                  </Table.Td>
-                ))}
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td fw={"bold"}>Errand</Table.Td>
-                {[353500, 407500, 331500].map((item, idx) => (
-                  <Table.Td key={idx}>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "IDR",
-                      minimumFractionDigits: 0,
-                    }).format(item)}
-                  </Table.Td>
-                ))}
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td fw={"bold"}>Utils</Table.Td>
-                {[1943000, 1743000, 1732500].map((item, idx) => (
-                  <Table.Td key={idx}>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "IDR",
-                      minimumFractionDigits: 0,
-                    }).format(item)}
-                  </Table.Td>
-                ))}
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td fw={"bold"}>Bensin</Table.Td>
-                {[120000, 160000, 160000].map((item, idx) => (
-                  <Table.Td key={idx}>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "IDR",
-                      minimumFractionDigits: 0,
-                    }).format(item)}
-                  </Table.Td>
-                ))}
-              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {dummy_non_essentials.map((item) => (
+                <Table.Tr key={item.category}>
+                  <Table.Td fw={"bold"}>{item.category}</Table.Td>
+                  {item.amount.map((item, idx) => (
+                    <Table.Td key={idx}>
+                      {new Intl.NumberFormat("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                        minimumFractionDigits: 0,
+                      }).format(item)}
+                    </Table.Td>
+                  ))}
+                </Table.Tr>
+              ))}
             </Table.Tbody>
           </Table>
         </Paper>
